fbc67c71974f6cd62757a43f52cd079bf49f3701
yogeshgaur001 <gauryogesh2003@gmail.com>
2025-03-22 22:43:48 +0530

    first commit

---
+++ src/component/home.js
@@ -0,0 +1,10 @@
+const Home=()=>{
+    return(
+        <>
+        <h1>Home Page hai ye mera
+        </h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/route.js
@@ -0,0 +1,13 @@
+import {useEffect} from "react";
+function Alert(){
+    useEffect(()=>{
+        console.log(alert('hey Mr. yogesh'));
+    })
+return(
+    <>
+    <h1>{"yogesh"}</h1>
+    
+    </>
+)
+}
+export default Alert
